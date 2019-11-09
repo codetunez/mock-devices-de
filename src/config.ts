e641369a02d9b8f327314ae1f50386b4837c0302
codetunez <codetunez@outlook.com>
2019-11-09 10:33:25 -0800

    Simplified config for DE

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,6 @@
 export class Config {
-
-    // PORTS
-    public static LOCALHOST: string = 'http://127.0.0.1';
     public static APP_PORT: string = '9100';
-
-    // LOGS
     public static WEBAPI_LOGGING: boolean = true;
     public static CONSOLE_LOGGING: boolean = true;
-
-    // UX
-    public static PROPERTY_WEBSOCKET_PORT: string = '9101';
-    public static CONSOLE_WEBSOCKET_PORT: string = '9102';
-    public static DEV_TOOLS: boolean = false;
-    public static APP_HEIGHT: number = 700;
-    public static APP_WIDTH: number = 890;
-    public static MAX_NUM_DEVICES: number = 750;
+    public static MAX_NUM_DEVICES: number = 1000;
 }
